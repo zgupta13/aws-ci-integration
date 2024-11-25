b6a4891d993de17eb049d25a25c65a7953fea59b
zgupta13 <shivangi.z.gupta@accenture.com>
2024-11-25 10:46:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
+terraform {
+ backend "s3" {
+ bucket = "shubhamtest1024"
+ key = "globalstate/s3/terraform.tfstate"
+ region = "eu-west-1"
+ # Replace this with your DynamoDB table name!
+ dynamodb_table = "Neeha-902903July-dblocks"
+ encrypt = true
+ }
+}
+
+
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
 }
