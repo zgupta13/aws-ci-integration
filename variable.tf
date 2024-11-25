3d35ef5f7c2f57c17e6ed744451bf6a339efd4b7
zgupta13 <shivangi.z.gupta@accenture.com>
2024-11-25 10:34:37 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,19 @@
+variable "s3_bucket_name" {
+  default     = "shivangiest1024"
+  description = "Name of the s3 bucket"
+  type        = string
+}
+variable "s3_tags" {
+  type = map
+
+  default = {
+    createdby   = "Shivangi"
+    environment = "dev"
+  }
+}
+
+variable "s3_regions" {
+  type    = string
+  default = "eu-west-1"
+}
 
