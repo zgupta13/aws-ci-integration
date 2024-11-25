7ebc5818bd470d0c89ab1251d8beb57b5ffdd93f
zgupta13 <shivangi.z.gupta@accenture.com>
2024-11-25 10:35:19 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,10 @@
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.bckt.arn
+  description = "arn of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.locks.name
+  description = "The name of the DynamoDB table"
+  
+}
